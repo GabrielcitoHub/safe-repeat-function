928d2f253cbc9237750735f10babdc7e86eacc8b
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2024-06-16 01:51:24 -0300

    Update safeRepeat.lua

--- safeRepeat.lua
+++ safeRepeat.lua
@@ -1,12 +1,18 @@
 -- Repeats the code the amount of time specified.
-function SafeRepeat(repeats, next)
+local function SafeRepeat(repeats, next)
     if repeats <= 9999 then --Change at your own risk.
       for i=1,repeats do
         next()
       end
     else
-        log("Amount of repeats is greater than the max limit, please lower it.")
+      local error = {
+        "",
+        { text = "safeRepeat: too many repeats! (max is 9999) \n",
+        color = "red",
+        },
+    }
+    printJson(toJson(error))
     end
 end
 
-return "<- what does this even do?"
+return SafeRepeat --<- i figured it out.
