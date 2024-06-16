19fd707876170bc649201ac31b3d8cc46c480aea
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2024-06-16 01:36:40 -0300

    Update safeRepeat.lua

--- safeRepeat.lua
+++ safeRepeat.lua
@@ -1,7 +1,11 @@
 -- Repeats the code the amount of time specified.
 function SafeRepeat(repeats, next)
-    for i=1,repeats do
+    if repeats <= 9999 then --Change at your own risk.
+      for i=1,repeats do
         next()
+      end
+    else
+        log("Amount of repeats is greater than the max limit, please lower it.")
     end
 end
 
